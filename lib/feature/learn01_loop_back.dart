fd30ac9e436a7f0a22cfc9581a8188ff9c76940a
Raghu Nandan <raghubs81@gmail.com>
2017-09-15 18:04:13 +0530

    modified:   lib/app_component.css 	modified:   lib/feature/learn01_loop_back.dart 	modified:   web/styles.css

--- lib/feature/learn01_loop_back.dart
+++ lib/feature/learn01_loop_back.dart
@@ -1,20 +1,39 @@
+import 'package:angular/core.dart';
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
+import 'package:angular_forms/angular_forms.dart';
 
 @Component
 (
   selector: 'learn01-loop-back',
   template:
   '''
-  <div class="container">
-    <h1>Loop back without going to the component</h1>
-    <input #box (keyup)="0">
-    <p>{{box.value}}</p>
-  </div>
-  '''
+  <form #heroForm="ngForm">
+     <div class="container">
+       <h1>Loop back using ngModel</h1>
+
+       <!-- Not Working: method not found: 'get(dollar)value' (t2.get(dollar)value is not a function)
+       <material-input floatingLabel label="Name" #myBox></material-input><br/>
+       {{myBox.value}}</p>
+       -->
+
+       <material-input floatingLabel label="Name" [(ngModel)]="myName" ></material-input><br/>
+       {{myName}}<p>
+
+      <!--
+       <material-input #myBox></material-input><br/>
+       {{myBox.value}}</p>
+     -->
+
+     </div>
+  </form>
+  ''',
+  directives: const [CORE_DIRECTIVES, formDirectives, materialDirectives, MaterialInputComponent, MaterialMultilineInputComponent],
 )
 class Learn01LoopBack
 {
+   String myName = "";
+
    Learn01LoopBack ()
    {
       print ("In loop back logger");
